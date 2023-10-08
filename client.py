2ff0b20d7ebf8a69fbed8c4d0866568ae0770419
yite <coastq22889@icloud.com>
2023-10-08 19:04:39 +0800

    client

---
+++ client.py
@@ -0,0 +1,23 @@
+import grpc
+import exhost_pb2 as exhost_pb2
+import exhost_pb2_grpc as exhost_pb2_grpc
+
+class gRPCClient:
+    def __init__(self, host, port, logger):
+        self.logger = logger
+
+        self.channel = grpc.insecure_channel(f"{host}:{port}")
+        self.stub = exhost_pb2_grpc.ExchangeStub(self.channel)
+
+    def send_market_order(self, base, quote, side, qty, dry_run):
+        request = exhost_pb2.MarketOrderRequest(
+            base=base,
+            quote=quote,
+            side=side,
+            qty=qty,
+            dry_run=dry_run
+        )
+
+        response = self.stub.SendMarketOrder(request)
+        res_id = next(response).id
+        return res_id
