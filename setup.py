e9285fa50306393e32519b5199ce979f439d3c6d
yite <coastq22889@icloud.com>
2023-10-08 21:48:04 +0800

    fill grpc lib

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='exhost',
-    version='1.0',
+    version='1.0.0',
     description='client of grpc for user send order',
     author='Yi Te',
     author_email='coastq22889@icloud.com',
     packages=find_packages(),
     install_requires=[
-        'git+https://github.com/yitech/coin-infra.git',
+        'grpcio'
+        'grpcio-tools'
+        'coin-infra',
     ],
 )
